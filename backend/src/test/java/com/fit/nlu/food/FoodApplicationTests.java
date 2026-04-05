26593c6635c156e2839b3c0dae8a956b47ef6725
Nguyễn Thanh Sơn <22130237@st.hcmuaf.edu.vn>
2026-04-05 22:22:18 +0700

    First commit

---
+++ backend/src/test/java/com/fit/nlu/food/FoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.nlu.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
